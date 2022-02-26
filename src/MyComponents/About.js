5560422cba45326be60eb8a9e35e7c5107d587a9
VIKASH\vikash <vikash0cv@gmail.com>
2022-02-26 20:58:54 +0530

    my commit

---
+++ src/MyComponents/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const About = () => {
+  return (
+    <div>
+        This app is not a garbage app..... :)
+        
+    </div>
+  )
+}
---
+++ src/MyComponents/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const Footer = () => {
+    
+  return (
+    <footer className='bg-dark text-light py-1  ' >
+        <p className="text-center" >
+            Copyright &copy; Vikash Kumar
+        </p>
+        
+    </footer>
+  )
+}
